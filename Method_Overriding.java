c7a89e909ccdc901470a89b4cf0ac00667707228
Ruturaj-007 <pruturaj3003@gmail.com>
2025-09-28 11:28:21 +0530

    feat: add Method Overriding

---
+++ Method_Overriding.java
@@ -0,0 +1,40 @@
+package sem_exm;
+
+// Method Overriding / Run-Time Polymorphism
+
+class Bank {
+    int getRateOfInterest() {
+        return 0;
+    }
+}
+class SBI extends Bank {
+    @Override
+    int getRateOfInterest() {
+        return 5;
+    }
+}
+
+class ICCI extends Bank {
+    @Override
+    int getRateOfInterest() {
+        return 11;
+    }
+}
+
+class AXIS extends Bank {
+    @Override
+    int getRateOfInterest() {
+        return 9;
+    }
+}
+public class Method_Overriding {
+    public static void main(String[] args) {
+        SBI s = new SBI();
+        ICCI I = new ICCI();
+        AXIS A = new AXIS();
+
+        System.out.println(I.getRateOfInterest());
+        System.out.println(s.getRateOfInterest());
+        System.out.println(A.getRateOfInterest());
+    }
+}
